19bb5544ed676de2d6deb4263a96dc1e47b734f7
DamianoSamperi <102314677+DamianoSamperi@users.noreply.github.com>
2025-10-10 14:04:37 +0200

    Update topography.py

--- build/topography_tool/topography.py
+++ build/topography_tool/topography.py
@@ -109,10 +109,11 @@ def generate_deployments(steps: List[Dict], pipeline_prefix: str, namespace="def
         deployment_name = f"{pipeline_prefix}-step-{step_id}"
         node_selector = step.get("nodeSelector") or {}
         #docker hub images: "dami00/multicomponent_service"
-        if node_selector.get("kubernetes.io/hostname") == "jetsonorigin":
-            base_image = "192.168.1.252:480/jetson/multicomponent_service:r36"
-        else:
-            base_image = "192.168.1.252:480/jetson/multicomponent_service:latest"
+        #if node_selector.get("kubernetes.io/hostname") == "jetsonorigin":
+        #    base_image = "192.168.1.252:480/jetson/multicomponent_service:r36"
+        #else:
+        #    base_image = "192.168.1.252:480/jetson/multicomponent_service:latest"
+        base_image = "192.168.1.252:480/jetson/multicomponent_service:latest"
         # container base
         container = {
             "name": "nn",
